f780eebe9f3c3cdacbc73639744c1443a2521a9a
Denittka <dan.belokon@gmail.com>
2020-02-01 21:17:16 +0300

    Исправил отрисовку.

--- Classes/Board/__init__.py
+++ Classes/Board/__init__.py
@@ -22,9 +22,10 @@ class Board:
         self.run()
 
     def update(self):
-        for line in board:
+        for line in self.board:
             for cell in line:
-                cell.update()
+                if isinstance(cell, Cell):
+                    cell.update()
 
     def search_on_board(self, mouse_pos):
         """
@@ -92,5 +93,6 @@ class Board:
                         if isinstance(coords, list):
                             self.board[coords[1]][coords[0]] = Cell(self, coords[0], coords[1])
             self.update()
+            screen.fill((0, 0, 0))
             self.board_render(screen)
             pygame.display.flip()
