0d01df8b6349b747583a84e1a489c7a2fdf44822
AzyaFLEX <52008529+AzyaFLEX@users.noreply.github.com>
2020-02-02 17:13:08 +0300

    Повороты влево и вправо

--- Classes/Cell/__init__.py
+++ Classes/Cell/__init__.py
@@ -1,25 +1,60 @@
+from random import randint
+
+
 class Cell:
     """
     Класс клетки -- живого существа, живущего по собственному генному коду.
     """
-    def __init__(self, board):
+    def __init__(self, board, x, y):
         """
         Принимает на вход изначальную доску, чтобы взаимодейсвтует с миром.
         :param board: изначальная доска
         """
+        self.x = x
+        self.y = y
         self.board = board
-        self.code = []  # TODO написать
+        self.code = [2, 2, 2, 2, 2, 2, 2, 2, 2, 5]  # TODO написать
         self.step = 0
 
     def move(self, direction):
         if direction == "UP":
-            pass  # TODO
+            if self.y > 0:
+                if self.board.board[self.y - 1][self.x] is None:
+                    self.board.board[self.y - 1][self.x] = self
+                    self.board.board[self.y][self.x] = None
+                    self.y -= 1
         if direction == "RIGHT":
-            pass  # TODO
+            if self.x < len(self.board.board[0]) - 1:
+                if self.board.board[self.y][self.x + 1] is None:
+                    self.board.board[self.y][self.x + 1] = self
+                    self.board.board[self.y][self.x] = None
+                    self.x += 1
         if direction == "DOWN":
-            pass  # TODO
+            if self.y < len(self.board.board) - 1:
+                if self.board.board[self.y + 1][self.x] is None:
+                    self.board.board[self.y + 1][self.x] = self
+                    self.board.board[self.y][self.x] = None
+                    self.y += 1
         if direction == "LEFT":
-            pass  # TODO
+            if self.x > 0:
+                if self.board.board[self.y][self.x - 1] is None:
+                    self.board.board[self.y][self.x - 1] = self
+                    self.board.board[self.y][self.x] = None
+                    self.x -= 1
+        if direction == "TLEFT":
+            for i in range(len(self.code)):
+                if self.code[i] in {1, 2, 3, 4}:
+                    if self.code[i] != 1:
+                        self.code[i] -= 1
+                    else:
+                        self.code[i] = 4
+        if direction == "TRIGHT":
+            for i in range(len(self.code)):
+                 if self.code[i] in {1, 2, 3, 4}:
+                    if self.code[i] != 4:
+                        self.code[i] += 1
+                    else:
+                        self.code[i] = 1
 
     def do(self, command):
         if command == "MOVE UP":
@@ -30,6 +65,10 @@ class Cell:
             self.move("DOWN")
         if command == "MOVE RIGHT":
             self.move("RIGHT")
+        if command == "TURN TO LEFT":
+            self.move("TLEFT")
+        if command == "TURN TO RIGHT":
+            self.move("TRIGHT")
 
     def get(self, num):
         if num == 1:
@@ -40,6 +79,10 @@ class Cell:
             return "MOVE DOWN"
         if num == 4:
             return "MOVE LEFT"
+        if num == 5:
+            return "TURN TO LEFT"
+        if num == 6:
+            return "TURN TO RIGHT"
 
     def update(self):
         self.do(self.get(self.code[self.step]))
